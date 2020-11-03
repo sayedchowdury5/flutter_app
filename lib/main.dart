079f4e0ed66e78d2b45ed64494f2eb83ff808984
Abu Sayed <sayedchowdury5@gmail.com>
2020-11-03 18:18:56 +0800

    Add Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: RandomWords(),
     );
   }
